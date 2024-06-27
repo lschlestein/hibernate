9f52abf3896676f048ee99724415841b865c3de9
Lucas Schlestein <lschlestein@gmail.com>
2024-06-27 14:45:59 -0300

    UPDATE melhorando os comentários

--- src/main/java/JPA/Main.java
+++ src/main/java/JPA/Main.java
@@ -7,21 +7,20 @@ import JPA.Model.Funcionario;
 
 public class Main {
     public static void main(String[] args) {
+        //Cria uma instancia das DAOs para as entidades Departamento e Funcionário
         DepartamentoDAO departamentoDAO = new DepartamentoDAO();
         FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-        //departamentoDAO.save(new Departamento("Engenharia"));
+
+        //Grava um novo Departamento no banco de dados
+        departamentoDAO.save(new Departamento("Engenharia"));
+        //Busca um Departamento já existente por Id
         Departamento departamento = departamentoDAO.findById(2);
         System.out.println(departamentoDAO.findAll());
 
-        Funcionario funcionario = Funcionario.
-                builder().
-                nome("Paula").
-                salario(5000.0).
-                departamento(departamento).
-                build();
+        //Cria um novo Funcionário utilizando o Builder
+        Funcionario funcionario = Funcionario.builder().nome("Paula").salario(5000.0).departamento(departamento).build();
+        //Grava o novo Funcionário, e seu respectivo Departamento no banco de dados.
         funcionarioDAO.save(funcionario);
         System.out.println(funcionarioDAO.findAll());
-
-
     }
 }
